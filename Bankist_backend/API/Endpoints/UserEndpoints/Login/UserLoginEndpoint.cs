15dde17d0641de9cb402f9bcd9014a2a06ccd6b5
Faris Dizdarevic <faris.dizdarevic@edu.fit.ba>
2023-12-14 22:19:42 +0100

    Added login endpoint for user

---
+++ Bankist_backend/API/Endpoints/UserEndpoints/Login/UserLoginEndpoint.cs
@@ -0,0 +1,55 @@
+﻿using API.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Endpoints.UserEndpoints.Login
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserLoginEndpoint : MyBaseEndpoint<UserLoginRequest, UserLoginResponse>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserLoginEndpoint(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public override async Task<UserLoginResponse> Procces([FromBody] UserLoginRequest request, CancellationToken cancellationToken)
+        {
+            User? user = await _dbContext.User.FirstOrDefaultAsync(u => u.userName == request.username);
+
+            if (user == null)
+            {
+                return new UserLoginResponse
+                {
+                    statusCode = 404,
+                    message = "User not found"
+                };
+            }
+
+            if (user.password != request.password)
+            {
+                return new UserLoginResponse
+                {
+                    statusCode = 401,
+                    message = "Incorrect password"
+                };
+            }
+
+            return new UserLoginResponse
+            {
+                statusCode = 200,
+                message = "Successfully logged in!"
+            };
+        }
+    }
+}
---
+++ Bankist_backend/API/Endpoints/UserEndpoints/Login/UserLoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Endpoints.UserEndpoints.Login
+{
+    public class UserLoginResponse
+    {
+        public int statusCode { get; set; }
+        public string message { get; set; }
+
+    }
+}
